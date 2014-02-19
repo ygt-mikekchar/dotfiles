06d5946df3cd0074377869d640980d2d7998176c
Mike Charlton <mike.charlton@yourgolftravel.com>
2014-02-19 14:25:28 +0000

    Add VNC files and improve install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 # am making symlinks
 
 echo "Removing existing files"
-for i in ~/.bashrc ~/.thymerc ~/.tmux.conf ~/.vimrc ~/.config/awesome/rc.lua; do
+for i in ~/.bashrc ~/.thymerc ~/.tmux.conf ~/.vimrc ~/.config/awesome/* ~/.vnc/*; do
   echo "  removing $i"
   rm -f $i
 done
 
 echo "Creating directories"
 # Create dirs if they don't already exist
-for i in ~/.config ~/.config/awesome; do
+for i in ~/.config ~/.config/awesome ~/.vnc; do
   if [ -d $i ]; then
     echo "  $i already exists"
   else
@@ -25,11 +25,21 @@ for i in .bashrc .thymerc .tmux.conf .vimrc; do
   echo "  linking $i"
   ln -s `pwd`"/$i" ~
 done
+for i in .config/awesome/*; do
+  echo "  linking $i"
+  ln -s `pwd`"/$i" ~/.config/awesome/
+done
+for i in .vnc/*; do
+  echo "  linking $i"
+  ln -s `pwd`"/$i" ~/.vnc
+done
 
-echo "Linking bin files into /usr/local/bin"
+# Copying executables because things like startup shells
+# don't like links
+echo "Copying bin files into /usr/local/bin"
 cd bin
 for i in *; do
-  echo "  linking bin/$i"
+  echo "  copying bin/$i"
   sudo rm -f "/usr/local/bin/$i"
-  sudo ln -s `pwd`"/$i" /usr/local/bin
+  sudo cp `pwd`"/$i" /usr/local/bin
 done
