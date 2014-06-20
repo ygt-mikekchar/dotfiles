782d4ff19b381ba087e7ee74b5d9d4a8be40acce
Mike Charlton <mike.charlton@yourgolftravel.com>
2014-06-20 15:36:22 +0100

    Put the systray on the last screen in Awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -200,7 +200,8 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    if s == 1 then right_layout:add(wibox.widget.systray()) end
+    -- MKC: Add the systray to last screen
+    if s == screen.count() then right_layout:add(wibox.widget.systray()) end
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
