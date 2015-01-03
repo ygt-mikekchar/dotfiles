e58573079c2e9f811187916b35e4da9f8b819360
Mike Charlton <mike.charlton@yourgolftravel.com>
2015-01-03 21:03:28 +0900

    Completely change the install

--- install.sh
+++ install.sh
@@ -1,53 +1,57 @@
 #!/bin/sh
 
-# Remove any files that already exist.  I have to do this because I
-# am making symlinks
-
-echo "Removing existing files"
-for i in ~/.bashrc ~/.bash_aliases ~/.thymerc ~/.tmux.conf ~/.vimrc ~/.vim ~/.config/awesome/* ~/.vnc/* ~/.xinitrc ~/.emacs.d/init.el ~/.gnupg/*.conf ~/.gitconfig ~/.gitignore_local; do
-  echo "  removing $i"
-  rm -f $i
-done
-
-echo "Creating directories"
-# Create dirs if they don't already exist
-for i in ~/.config ~/.config/awesome ~/.vnc ~/.emacs.d ~/.gnupg; do
-  if [ -d $i ]; then
-    echo "  $i already exists"
+link_file() {
+  FILE="$1"
+  DEST="$2"
+  if [ -h "$DEST/$FILE" ]; then
+    echo "Removing old $DEST/$FILE"
+    rm "$DEST/$FILE"
+  fi 
+  if [ -e "$DEST/$FILE" ]; then
+    echo "$DEST/$FILE exists and is not a link.  Skipping."
   else
-    echo "  creating $i"
-    mkdir $i
+    echo "Linking $DEST/$FILE"
+    ln -s `pwd`"/$FILE" "$DEST/$FILE"
   fi
-done
+}
 
-echo "Making symbolic links"
-for i in .bashrc .bash_aliases .thymerc .tmux.conf .vimrc .xinitrc .vim .gitconfig .gitignore_local; do
-  echo "  linking $i"
-  ln -s `pwd`"/$i" ~
-done
-for i in .config/awesome/*; do
-  echo "  linking $i"
-  ln -s `pwd`"/$i" ~/.config/awesome/
-done
-for i in .vnc/*; do
-  echo "  linking $i"
-  ln -s `pwd`"/$i" ~/.vnc
-done
-for i in .emacs.d/*; do
-  echo "  linking $i"
-  ln -s `pwd`"/$i" ~/.emacs.d
-done
-for i in .gnupg/*.conf; do
-  echo "  linking $i"
-  ln -s `pwd`"/$i" ~/.gnupg
-done
+link_files() {
+  FILE="$1"
+  DEST="$2"
+  if [ -d "$FILE" ]; then
+    link_directory "$FILE" "$DEST"
+  else
+    link_file "$FILE" "$DEST"
+  fi
+}
 
-# Copying executables because things like startup shells
-# don't like links
-echo "Copying bin files into /usr/local/bin"
-cd bin
-for i in *; do
-  echo "  copying bin/$i"
-  sudo rm -f "/usr/local/bin/$i"
-  sudo cp `pwd`"/$i" /usr/local/bin
-done
+link_directory() {
+  FILE="$1"
+  DEST="$2"
+  if [ ! -e "$DEST/$FILE" ]; then
+    echo "Creating directory $DEST/$FILE"
+    mkdir "$DEST/$FILE"
+  else
+    echo "$DEST/$FILE already exists"
+  fi
+  if [ ! -d "$DEST/$FILE" ]; then
+    echo "$DEST/$FILE is not a directory.  Skipping."
+  else
+    for i in "$FILE"/*; do
+      if [ -e "$i" ]; then
+        link_files "$i" "$DEST"
+      fi
+    done
+    for i in "$FILE"/.[^.]*; do
+      if [ -e "$i" ]; then
+        link_files "$i" "$DEST"
+      fi
+    done
+  fi
+}
+
+pushd home
+link_directory mikekchar /home
+link_file .vim /home/mikekchar
+popd
+
