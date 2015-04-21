78b9dd9a2984a1b5fa3b861c33324661115faaed
Mike Charlton <mike.charlton@yourgolftravel.com>
2015-04-21 14:41:31 +0900

    Add evil emacs configuration

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,5 +1,67 @@
-; Load ledger mode
-(add-to-list 'load-path
-             (expand-file-name "/home/mike/devel/ledger/lisp"))
-(load "ledger-mode")
-(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+;; disable backup and autosave
+(setq backup-inhibited t)
+(setq auto-save-default nil)
+
+;; turn off menu bar
+(menu-bar-mode -1)
+
+;; set indentation
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'ruby-indent-level 'tab-width)
+(defvaralias 'js-indent-level 'tab-width)
+(defvaralias 'coffee-tab-width 'tab-width)
+
+;; Use the directory where Emacs was first started as the
+;; directory to open new files (the same as Vim)
+(add-hook 'find-file-hook
+          (lambda ()
+            (setq default-directory command-line-default-directory)))
+
+;; packages
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")))
+(package-initialize)
+
+(defun require-package (package)
+  (setq-default highlight-tabs t)
+  "Install given PACKAGE."
+  (unless (package-installed-p package)
+    (unless (assoc package package-archive-contents)
+      (package-refresh-contents))
+    (package-install package)))
+
+(require-package 'ledger-mode)
+(require-package 'evil)
+(eval-after-load 'evil-ex
+  '(evil-ex-define-cmd "b[wipeout]" 'kill-buffer))
+
+(require-package 'magit)
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+(require-package 'coffee-mode)
+
+(require-package 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Turn on Evil mode
+(evil-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (agnostic)))
+ '(custom-safe-themes
+   (quote
+    ("df61a8a15e9aac1f9925c452adfa532ec14f07ab40d7097617a0e7cbb4456fb6" "f89abc033d8b27200f38ecb7e3dd9b9ae6100211cdd7e489c1e86318dbaa0a9e" "537cfac1c3b28b04bc41341abb5857f9549905718abb540fe67accd7b3bdb73f" "ad6d43807539295bb05aab6577b41776deaff984aede94432961b2de59265abf" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
