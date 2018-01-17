2175d7e2ec9e22aa80d94071f244daadb5b35513
Mike Charlton <mike.charlton@yourgolftravel.com>
2018-01-17 11:09:44 +0000

    List ppas on the vm

---
+++ add_ppas.sh
@@ -0,0 +1,6 @@
+sudo apt-add-repository ppa:openjdk-r/ppa
+sudo apt-add-repository ppa:brightbox/ruby-ng-experimental
+sudo apt-add-repository ppa:ubuntu-elisp/ppa
+sudo apt-add-repository ppa:guacamole/stable
+sudo apt-add-repository ppa:chris-lea/node
+sudo apt-add-repository ppa:git-core/ppa
