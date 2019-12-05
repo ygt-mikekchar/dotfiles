e5decd3df526205a73ddd719c1e56621bf501fa5
Mike Charlton <mike.charlton@yourgolftravel.com>
2019-12-05 15:56:16 +0900

    Stuff to make it work
    Please make it work!

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -30,7 +30,9 @@
   (mapc 'kill-buffer (buffer-list)))
 
 (require 'package)
-(require 'use-package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+;;(require 'use-package)
 
 ;; disable backup and autosave
 (setq backup-inhibited t)
@@ -246,8 +248,6 @@
    (quote
     ("f0352ed0b1c5ca5159e5de89c1456cbac9d582ce7c8600c786b90200048c7496" "568c0f257e444904c3d9ec7174c958bed4dec19684964f6d9b689544a1a9a6ae" "24eabb04960aa2b52371307b373ee9efc91c8c98d137448e7c023c18d79ce86c" "f1ce2f12ddfc4ea2be9d6ea997971d59f136b78930e0c3bd07a8bdb7d639c6d9" "7779e67feefc8a42ce9cc65e2a4e1afad79718a9d6fd9a28095a9132a62e995f" "1fa142653c10fa72a8fda151f733b7b2ae4bb26a72899e60ea9cd01a24d4023a" "3ae63b5300acc68006a0a9cf251907811182566e2ff992453a7016d7e4d431b7" "1722b8321e082e903006732d1b4a890dc8bf994e5f49df9c96442e798db77c1b" "cd314e07214617a4cf31b7f89c26f2f582bea0168e32ccb59a9b611d39d67956" "6d012ea4d872f0f3e3cd9ef2784ca3a699097fc704337e5ba090c918c5ae31c9" "684e38c64081cd699bb6a723cdeb2fd49bb1ae767aaf0dcb63ba10e28630a985" "c9f3de9842cb5a1244fb79354c415b4f64aac85ccec51111be0260773362fe66" "ae9a2953025f3c2837d35ad2f7910c2424063ae48f13293774d43c90a6dca15e" "6603c34e81fd25b59d6329e4e8d0b083fcd24c50ddcdd94bb4fcd2ccc8da0263" "e39769e198080195c07f3a08dad352298c4c1085b0c5266883bbc8b9db9cb11c" "2af32cc56ac6e544c9661ce4ab60ffe55940a1564031b91af8989c66cf62f177" "81f31984aa99ab4b1be4a73f7b5a2e2d134a8e3107266bd8fb93683d59d8c0ce" "8189204b074586475ac8d1be028bbdc4e0d1256cf2c063b706b58c38b5173016" "a1ed894e04510d07b4b6cf03267258357afdac6c7d824ab5d45428eda2d57a0a" "bf44f7d22051ac9478aa153ba50199ed0fbe359e1c1854fe7440dc000a6e1228" "cccd52d7943e895c3c850d9f49298872cb57c51f5b73349ef3b2bc3283b9f3f5" "362d2dc4e0fa755e0c1009ed0c37a206c92dc7bda0ded39cec8df32d13a98b0a" "c8b27c432c3983f4e703c592327bf0d10ff890e3054d47ee471c5f7d4dc5aef8" "65f8d6345f5898b1ee86b885fd61e2ce85f3589722e0cabe8df7cb4daa3e88b5" "81d75a80856e7368952dd3408c6cdb6935f1d6199579d372ae9004b7307bbe4b" "736e0868e57d6e29f3b1cc29c8edc7b62692ae4e1dd1429f9ecd934ba0f8a697" "5d97cce1063d40c95eda54bb69eb42ed05e839313d181c49464679e38285232d" "5d92dcbb0ceb8fe059732fce6d433c77ec3f8b439e9e2f6bbfb947df89b89979" "f51a4d4328c4a235942d819cd71f6b0c761296bc2cfc2cde44f33bbfc0711e05" "df61a8a15e9aac1f9925c452adfa532ec14f07ab40d7097617a0e7cbb4456fb6" "f89abc033d8b27200f38ecb7e3dd9b9ae6100211cdd7e489c1e86318dbaa0a9e" "537cfac1c3b28b04bc41341abb5857f9549905718abb540fe67accd7b3bdb73f" "ad6d43807539295bb05aab6577b41776deaff984aede94432961b2de59265abf" default)))
  '(js-indent-level 2)
- '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
- '(org-trello-files (quote ("/home/mikekchar/work/trello/sales.org")) nil (org-trello))
  '(package-selected-packages
    (quote
     (flycheck-haskell psc-ide flycheck-elm flycheck-rust evil-magit rust-mode add-node-modules-path markdown-mode vue-mode web-mode company-go go-mode haskell-mode company eslint-fix js2-mode jtags purescript-mode org-trello elm-mode enh-ruby-mode magit ledger-mode evil editorconfig coffee-mode)))
